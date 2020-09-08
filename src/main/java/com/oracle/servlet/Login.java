826093994deb29551f15a5ff87a5a58c77edea43
angelicxia <2546646340@qq.com>
2020-09-08 11:49:28 +0800

    jjj

---
+++ src/main/java/com/oracle/servlet/Login.java
@@ -0,0 +1,25 @@
+package com.oracle.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        String name=req.getParameter("uname");
+        String pass =req.getParameter("upass");
+        resp.getWriter().write(name+"\t"+pass);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
